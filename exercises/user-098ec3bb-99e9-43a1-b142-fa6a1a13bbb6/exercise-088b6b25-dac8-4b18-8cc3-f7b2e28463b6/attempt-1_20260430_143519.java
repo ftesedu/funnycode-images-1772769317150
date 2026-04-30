11c4f795a2e4cd1c0084b0a33b97fcdba7893fba
FTES EDUCATION <ai-learning@ftes.vn>
2026-04-30 14:35:20 +0700

    Submit exercise: user-098ec3bb-99e9-43a1-b142-fa6a1a13bbb6, exercise-088b6b25-dac8-4b18-8cc3-f7b2e28463b6, attempt-1

---
+++ exercises/user-098ec3bb-99e9-43a1-b142-fa6a1a13bbb6/exercise-088b6b25-dac8-4b18-8cc3-f7b2e28463b6/attempt-1_20260430_143519.java
@@ -0,0 +1,22 @@
+package Homework_Ftes_Nop;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Anh Tuan
+ */
+public class Homework_Ftes1_ThucHienPhepTinh2 {
+    
+    public static void main(String[] args) {
+        int a=5, b=10;
+        System.out.println("Tổng: "+(a+b)+" Hiệu: "+(a-b)+" Tích: "+(a*b)+" Thương: "+(a/b));
+        
+        
+    }
+    
+    
+}

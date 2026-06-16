c099bab898ce72ce1dacb7640df2630d84344412
FTES EDUCATION <ai-learning@ftes.vn>
2026-06-16 20:16:22 +0700

    Submit exercise: user-106b9112-9688-431a-9fc5-695697db74a3, exercise-4aadf4f8-e495-4b50-be80-398b21479fce, attempt-1

---
+++ exercises/user-106b9112-9688-431a-9fc5-695697db74a3/exercise-4aadf4f8-e495-4b50-be80-398b21479fce/attempt-1_20260616_201622.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+  private:
+    double radius;
+
+  public:
+    Circle(double r) {
+        this->radius = r;
+    }
+    double getArea() {
+        return 3.14 * radius * radius;
+    }
+    double getCircumference() {
+        return 2 * 3.14 * radius;
+    }
+};

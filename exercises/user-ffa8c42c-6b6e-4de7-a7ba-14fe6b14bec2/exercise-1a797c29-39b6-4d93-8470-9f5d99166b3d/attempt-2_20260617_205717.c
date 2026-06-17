7b73fa354268827ea0f1cb4bf8ceced2a1101669
FTES EDUCATION <ai-learning@ftes.vn>
2026-06-17 20:57:18 +0700

    Submit exercise: user-ffa8c42c-6b6e-4de7-a7ba-14fe6b14bec2, exercise-1a797c29-39b6-4d93-8470-9f5d99166b3d, attempt-2

---
+++ exercises/user-ffa8c42c-6b6e-4de7-a7ba-14fe6b14bec2/exercise-1a797c29-39b6-4d93-8470-9f5d99166b3d/attempt-2_20260617_205717.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void swap(int *x, int *y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main () {
+
+
+	int a, b;
+	printf("a = ");
+	scanf("%d", &a);
+	printf("\nb = ");
+	scanf("%d", &b);
+	swap(&a, &b);
+	printf("\nSau khi hoan doi:\n");
+	printf("a = %d\n", a);
+	printf("b = %d", b);
+	return 0;
+}

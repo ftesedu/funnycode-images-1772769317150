8b1c36c0b8208e5f359ce8375d137ddcfd9bd683
FTES EDUCATION <ai-learning@ftes.vn>
2026-06-21 08:46:38 +0700

    Submit exercise: user-0662bc4f-9d98-41d9-82e9-cdeb646f4f1e, exercise-8cebd8e5-73c7-42c4-9c5a-0fb8778f489d, attempt-1

---
+++ exercises/user-0662bc4f-9d98-41d9-82e9-cdeb646f4f1e/exercise-8cebd8e5-73c7-42c4-9c5a-0fb8778f489d/attempt-1_20260621_084637.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int val;
+    int sum = 0;
+    for(int i = 0;i < n;i++){
+        cin >> val;
+        sum += val;
+    }
+    cout << "Tong " << "= " << sum << '\n';
+    return 0;
+}

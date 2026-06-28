30f79ffe37ed26da19c4604b2d9cb380dcd9693a
FTES EDUCATION <ai-learning@ftes.vn>
2026-06-28 21:34:25 +0700

    Submit exercise: user-c00bc64f-4707-4b7e-82d6-875b7e39ebd5, exercise-f7a30a85-83f6-4971-8ac2-56c0df064002, attempt-1

---
+++ exercises/user-c00bc64f-4707-4b7e-82d6-875b7e39ebd5/exercise-f7a30a85-83f6-4971-8ac2-56c0df064002/attempt-1_20260628_213424.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> nextGreater(vector<int>& arr){
+   stack<int> s;
+   vector<int> res;
+   int n = arr.size();
+   for( int i = n-1;i>=0;i--){
+       if(s.empty()){
+        s.push(arr[i]);
+        res.push_back(-1);
+
+       }else{
+        while(!s.empty()){
+            if(arr[i]<s.top()){
+                res.push_back(s.top());
+                s.push(arr[i]);
+                break;
+            }else{
+                s.pop();
+            }
+        }
+        if(s.empty()){
+            res.push_back(-1);
+            s.push(arr[i]);
+        }
+       }
+   }
+   
+   reverse(res.begin(), res.end());
+   return res;
+}
